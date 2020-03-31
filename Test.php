932c420db760ecfcfe83bcf5ac1ad7c37f2d723c
Jecofrbo <62480562+Jecofrbo@users.noreply.github.com>
2020-03-31 23:35:03 +0200

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,7 +1,34 @@
-<?php  
-phpinfo();
+<?php
+/**
+ * inkludovani interfacu
+ */
+include 'class/iustrednyBooij.php'; 
+/**
+ * inkludovani abstraktni tridy
+ */
+include 'class/ustrednyBooij.php';  
+/**
+ * inkludovani dedici tridy
+ */
+include 'class/ustrednaBooij.php'; 
 
-
-
-
-
+/**
+ * atribut podle ktereho nastavime napeti
+ */
+$napetiBooij = 24; 
+/**
+ * instance tridy ustrednaBooij
+ */
+$newBooij = new ustrednaBooij(); 
+/**
+ * vypsani ve var dump constanty TYPE
+ */
+var_dump($newBooij::TYPE); 
+/**
+ * nastaveni atributu pomoci instacniho pristupu k fuknci
+ */
+$newBooij->setNapetiBooij($napetiBooij);      
+/**
+ * vypsani atributu ve var dump instancnim zpusobem
+ */
+var_dump($newBooij->getNapetiBooij());        
